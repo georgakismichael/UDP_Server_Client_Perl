7d3172ec75e454be426a6b416b620c2b024270e7
MikeGee <georgakismichael@gmail.com>
2016-06-13 20:50:00 +0300

    Added command line arguments for client

--- client.pl
+++ client.pl
@@ -2,20 +2,57 @@ use strict;
 use warnings;
 use IO::Socket;
 use String::CRC32;
+use Getopt::Long;
 
-my($udp_client_socket, $DESTIP, $PORTNO, $PROTO, $MAXSZ, $outgoing, $ServerRunning, $ack, $ack_str, $checksum);
+my($result, $udp_client_socket, $DESTIP, $PORTNO, $PROTO, $MAXSZ, $killserver, $load, $outgoing, $ServerRunning, $ack, $ack_str, $checksum);
 
-$PORTNO = 5152;
+$result = GetOptions (	
+	"host:s"   	=> \$DESTIP,
+	"port:i" 	=> \$PORTNO,
+	"load:i" 	=> \$load,
+	"size:i" 	=> \$MAXSZ,
+	"killserver:s" 	=> \$killserver,	
+);
+
+if(!($DESTIP))
+{
+	$DESTIP = '127.0.0.1';
+	print "Target IP argument missing. Assuming $DESTIP...\n";
+}
+
+if(!($PORTNO))
+{
+	$PORTNO = 5152;
+	print "Target Port argument missing. Assuming $PORTNO...\n";
+
+}
+
+if(!($load))
+{
+	$load = 100;
+	print "Number of messages argument missing. Assuming $load...\n";
+}
+
+if(!($MAXSZ))
+{
+	$MAXSZ = 1024;
+	print "Message size argument missing. Assuming $MAXSZ bytes...\n";
+}
+
+if(($killserver ne "YES") and ($killserver ne "NO"))
+{
+	$killserver = 1;
+	print "Server shutdown argument missing or invalid. Assuming YES...\n";
+}
+
 $PROTO = 'udp';
-$DESTIP = '127.0.0.1';
-$MAXSZ = 1024;
 
 print "Starting $PROTO client on port $PORTNO\n";
 
 $udp_client_socket = new IO::Socket::INET(PeerAddr => $DESTIP, PeerPort => $PORTNO, Proto => $PROTO)
 or die "Couldn't create a $PROTO client on port $PORTNO : $@\n";
 
-print "Ready to sent $PROTO messages to port $PORTNO\n";
+print "Ready to sent $PROTO messages to $DESTIP port $PORTNO\n";
 
 $ServerRunning = 0;
 
@@ -23,14 +60,20 @@ while(!$ServerRunning)
 {
 	$outgoing = "hello";
 	$udp_client_socket->send($outgoing);
-	$udp_client_socket->recv($ack, $MAXSZ);
 	
-	my($ack_str, $checksum) = split(/_/, $ack);
-	
-	if(($ack_str eq "ACK") and ($checksum == crc32($outgoing)))
+	if($udp_client_socket->recv($ack, $MAXSZ))
 	{
-		print ("Server is alive!\n");
-		$ServerRunning = 1;
+		my($ack_str, $checksum) = split(/_/, $ack);
+		
+		if(($ack_str eq "ACK") and ($checksum == crc32($outgoing)))
+		{
+			print ("Server is alive!\n");
+			$ServerRunning = 1;
+		}
+		else
+		{
+			die ("Server handshake did not complete successfully. Exiting...\n");
+		}	
 	}
 	else
 	{
@@ -41,7 +84,7 @@ while(!$ServerRunning)
 
 my($pkts_fail, $pkts_ok, $stats) =(0, 0);
 
-for($a=0;$a<10; $a+=1)
+for($a=0;$a<$load; $a+=1)
 {
 	$outgoing = join'', map +(0..9,'a'..'z','A'..'Z')[rand(10+26*2)], 1..$MAXSZ;
 
@@ -64,11 +107,14 @@ $stats = ($pkts_ok/($pkts_ok + $pkts_fail))*100;
 
 print "$pkts_fail packets failed and $pkts_ok packets were transmitted properly ($stats%)\n";
 
-$outgoing = "killmenow";
+if($killserver eq "YES")
+{
+	$outgoing = "killmenow";
 
-$udp_client_socket->send($outgoing);
-$udp_client_socket->recv($ack, $MAXSZ);
+	$udp_client_socket->send($outgoing);
+	$udp_client_socket->recv($ack, $MAXSZ);
 
-sleep(2);
+	sleep(2);
+}
 
 $udp_client_socket -> close();
--- run_client_win.cmd
+++ run_client_win.cmd
@@ -1,2 +1,2 @@
-C:\Dwimperl\perl\bin\perl.exe client.pl
+C:\Dwimperl\perl\bin\perl.exe client.pl -host 127.0.0.1 -port 5152 -load 50 -size 100 -killserver NO
 PAUSE
