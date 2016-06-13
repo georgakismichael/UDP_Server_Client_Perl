d65522c950bcbf1b8bac0e2c2d5f9b2aff9ad400
MikeGee <georgakismichael@gmail.com>
2016-06-13 15:07:53 +0300

    Added super simple communication between client and server

--- client.pl
+++ client.pl
@@ -2,14 +2,25 @@ use strict;
 use warnings;
 use IO::Socket;
 
-my($udp_client_socket, $PORTNO, $PROTO);
+my($udp_client_socket, $DESTIP, $PORTNO, $PROTO, $outgoing, $ack);
 
 $PORTNO = 5151;
 $PROTO = 'udp';
+$DESTIP = '127.0.0.1';
 
 print "Starting $PROTO client on port $PORTNO\n";
 
-$udp_client_socket = new IO::Socket::INET(PeerAddr => '127.0.0.1', PeerPort => $PORTNO, Proto => $PROTO)
+$udp_client_socket = new IO::Socket::INET(PeerAddr => $DESTIP, PeerPort => $PORTNO, Proto => $PROTO)
 or die "Couldn't create a $PROTO client on port $PORTNO : $@\n";
 
 print "Ready to sent $PROTO messages to port $PORTNO\n";
+
+$outgoing = "asdasadasd";
+$udp_client_socket->send($outgoing);
+
+#read operation
+$ack = <$udp_client_socket>;
+print "Data received from socket : $ack\n ";
+
+sleep(10);
+$udp_client_socket->close();
--- server.pl
+++ server.pl
@@ -2,14 +2,27 @@ use strict;
 use warnings;
 use IO::Socket;
 
-my($udp_server_socket, $PORTNO, $PROTO);
+my($udp_server_socket, $PORTNO, $PROTO, $incoming, $from_address, $from_port);
 
 $PORTNO = 5151;
 $PROTO = 'udp';
 
-print "Starting $PROTO server on port $PORTNO\n";
+print "Starting $PROTO server on port $PORTNO... ";
 
 $udp_server_socket = new IO::Socket::INET(LocalHost => '127.0.0.1', LocalPort => $PORTNO, Proto => $PROTO)
 or die "Couldn't create a $PROTO server on port $PORTNO : $@\n";
 
+print "OK\n";
+
 print "Awaiting $PROTO messages on port $PORTNO\n";
+
+while(1)
+{
+	# read operation on the socket
+	$udp_server_socket->recv($incoming, 1024);
+
+	#get the peerhost and peerport at which the recent data received.
+	$from_address = $udp_server_socket->peerhost();
+	$from_port = $udp_server_socket->peerport();
+	print "\n($from_address , $from_port) said : $incoming";
+}
