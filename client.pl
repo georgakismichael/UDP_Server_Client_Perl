1f40d79dcc4de286f4a68f0b85b4d46479feeba8
MikeGee <georgakismichael@gmail.com>
2016-06-13 15:45:56 +0300

    Added ACKs and some basic message exchange

--- client.pl
+++ client.pl
@@ -2,11 +2,12 @@ use strict;
 use warnings;
 use IO::Socket;
 
-my($udp_client_socket, $DESTIP, $PORTNO, $PROTO, $outgoing, $ack);
+my($udp_client_socket, $DESTIP, $PORTNO, $PROTO, $MAXSZ, $outgoing, $ServerRunning, $ack);
 
 $PORTNO = 5151;
 $PROTO = 'udp';
 $DESTIP = '127.0.0.1';
+$MAXSZ = 1024;
 
 print "Starting $PROTO client on port $PORTNO\n";
 
@@ -15,12 +16,47 @@ or die "Couldn't create a $PROTO client on port $PORTNO : $@\n";
 
 print "Ready to sent $PROTO messages to port $PORTNO\n";
 
-$outgoing = "asdasadasd";
+$ServerRunning = 0;
+
+while(!$ServerRunning)
+{
+	$outgoing = "hello";
+	$udp_client_socket->send($outgoing);
+	$udp_client_socket->recv($ack, $MAXSZ);
+
+	if($ack eq "ACK")
+	{
+		print ("Server is alive!\n");
+		$ServerRunning = 1;
+	}
+	else
+	{
+		print ("Waiting for server...\n");
+		sleep(2);
+	}	
+}
+
+$outgoing = "hello";
 $udp_client_socket->send($outgoing);
+$udp_client_socket->recv($ack, $MAXSZ);
+print "Data received from socket : $ack\n";
 
-#read operation
-$ack = <$udp_client_socket>;
-print "Data received from socket : $ack\n ";
+sleep(2);
 
-sleep(10);
-$udp_client_socket->close();
+$outgoing = "mate!";
+$udp_client_socket->send($outgoing);
+$udp_client_socket->recv($ack, $MAXSZ);
+print "Data received from socket : $ack\n";
+
+sleep(2);
+
+$outgoing = "killmenow";
+$udp_client_socket->send($outgoing);
+$udp_client_socket->recv($ack, $MAXSZ);
+print "Data received from socket : $ack\n";
+
+sleep(2);
+
+
+
+$udp_client_socket -> close();
--- server.pl
+++ server.pl
@@ -2,10 +2,11 @@ use strict;
 use warnings;
 use IO::Socket;
 
-my($udp_server_socket, $PORTNO, $PROTO, $incoming, $from_address, $from_port);
+my($udp_server_socket, $PORTNO, $PROTO, $MAXSZ, $incoming, $from_address, $from_port, $ServerRunning, $ack);
 
 $PORTNO = 5151;
 $PROTO = 'udp';
+$MAXSZ = 1024;
 
 print "Starting $PROTO server on port $PORTNO... ";
 
@@ -16,13 +17,27 @@ print "OK\n";
 
 print "Awaiting $PROTO messages on port $PORTNO\n";
 
-while(1)
+$ServerRunning = 1;
+
+while($ServerRunning)
 {
 	# read operation on the socket
-	$udp_server_socket->recv($incoming, 1024);
+	$udp_server_socket->recv($incoming, $MAXSZ);
 
 	#get the peerhost and peerport at which the recent data received.
 	$from_address = $udp_server_socket->peerhost();
 	$from_port = $udp_server_socket->peerport();
-	print "\n($from_address , $from_port) said : $incoming";
+	print "\n($from_address , $from_port) said : $incoming\n";
+
+	$ack = "ACK";
+	$udp_server_socket->send($ack);
+	print "Sent $ack to $from_address:$from_port\n";
+	
+	if($incoming eq "killmenow")
+	{
+		print ("\nServer kill message recieved from $from_address. Stopping server...\n");
+		$ServerRunning = 0;
+	}	
 }
+
+$udp_server_socket -> close;
